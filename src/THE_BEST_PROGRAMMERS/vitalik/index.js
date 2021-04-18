377d39ea9f1c80e65b5e8760b1ddc9d616cf5ea0
vitalik57 <77902972+vitalik57@users.noreply.github.com>
2021-04-18 18:24:35 +0300

    закинув функцію і графік

---
+++ src/THE_BEST_PROGRAMMERS/vitalik/index.js
@@ -0,0 +1,49 @@
+// Изображения подключать так
+// import imgSource from "./images/arms.jpg"
+// в imgSource находится путь к вашему изображению
+// А СПРАЙТ вот так
+// document.body.insertAdjacentHTML("beforeend", `<svg>
+//         <use href="./sprite.svg#icon-clouds-and-sun"></use>
+//     </svg>`)
+rendChart(data);
+export default rendChart
+const arr = ['16.04','17.04','18.04','19.04','20.04']
+new Chart(document.getElementById("line-chart"), {
+  type: 'line',
+  data: {
+    labels: [...arr],
+    datasets: [{
+      
+      data: [86, 114, 106, 106, 107, 111, 133, 221, 783, 2478],
+  
+        label: "— Temperature, C° ",
+        borderColor: "#FF6B09",
+        fill: false
+      }, { 
+        data: [282,350,411,502,635,809,947,1402,3700,5267],
+        label: "— Humidity, % ",
+        borderColor: "#0906EB",
+        fill: false,
+        
+      }, { 
+        data: [168,170,178,190,203,276,408,547,675,734],
+        label: "— Wind Speed, m/s ",
+        borderColor: "#EA9A05",
+        fill: false
+      }, { 
+        data: [40,20,10,16,24,38,74,167,508,784],
+        label: "— Atmosphere Pressure, m/m",
+        borderColor: "#067806",
+        fill: false
+      }
+    ]
+  },
+  options: {
+        backgroundColor:"rgba(16, 33, 54, 0.8)",
+    title: {
+      display: true,
+      text: 'Hide Chart',
+      color:'#FF6B09'
+    }
+  }
+});
