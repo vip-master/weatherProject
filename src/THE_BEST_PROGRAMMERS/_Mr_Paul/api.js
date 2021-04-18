40b70fb746671226254faea0d41486bdab144e22
vip-master <1998.illia.z@gmail.com>
2021-04-18 19:16:24 +0300

    Delete api.js

--- src/THE_BEST_PROGRAMMERS/_Mr_Paul/api.js
+++
@@ -1,7 +0,0 @@
-function fetchCountries(countryName) {
-    return fetch(
-        `https://api.openweathermap.org/data/2.5/forecast?q=London&units=metric&appid=939eab3895e9f452425ca7e40505d9c8`,
-      ).then(res => res.json());
-  }
-  
-  export default fetchCountries;
