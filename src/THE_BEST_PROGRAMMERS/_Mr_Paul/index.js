6927a2746d9fa6a0859caf2d1a07f46ce601d798
MrPaul007 <75275133+MrPaul007@users.noreply.github.com>
2021-04-19 18:01:59 +0300

    nothing

--- src/THE_BEST_PROGRAMMERS/_Mr_Paul/index.js
+++ src/THE_BEST_PROGRAMMERS/_Mr_Paul/index.js
@@ -7,11 +7,114 @@
 //     </svg>`)
 import { rendMore } from '../serg/lib.js'
 
+// import daysCard from './daysCard.hbs';
+
+// const weatherList = document.querySelector('.weather-list')
+
+// document.body.insertAdjacentHTML("beforeend",
+// `<h2`)
+
+// const weekDays = {
+//     'Sun': 'Sunday',
+//     'Mon': 'Monday',
+//     'Tue': 'Tuesday',
+//     'Wed': 'Wednesday',
+//     'Thu': 'Thursday',
+//     'Fri': 'Friday',
+//     'Sat': 'Saturday',
+// }
+
 function rendMain(data) {
+    
+    console.log(data);
     rendMore(data)
-    console.log("wellcome from Ternopil");
+    // const dayOnCards = [];
+    // const dateOnCards = [];
+    // const monthOnCards = [];
+    // const iconOnCard = []; 
+    // for(let i = 0; i < data.length; i +=8) {
+    //     const justArr = data[i].dt.split(' ');
+    //     dayOnCards.push(justArr[0]);
+    //     dateOnCards.push(justArr[2]);
+    //     monthOnCards.push(justArr[1])
+    //     iconOnCard.push(data[i].weather[0].icon)
+    // };
+
+    // class Card {
+    //     constructor(day, date, month, icon) {
+    //         this.day = day;
+    //         this.date = date;
+    //         this.month = month;
+    //         this.icon = icon;
+
+    //     };
+    // }
+    // const daysArray = [];
+    // for(let i = 0; i < dayOnCards.length; i +=1) {
+    //     const oneDay = new Card(weekDays[dayOnCards[i]], dateOnCards[i], monthOnCards[i], iconOnCard[i]);
+    //     daysArray.push(oneDay);
+    // }
+    // console.log(iconOnCard);
+
+    // weatherList.innerHTML = daysCard(daysArray);
 };
+
+
 export { rendMain };
+
+// import markup from './markup.hbs';
+
+// const test = document.querySelector('.test')
+
+//     const moreInfoArray = splitArrayIntoChunks (data.list, 8);
+//     console.log(moreInfoArray);
+
+//     const dayOnCards = [];
+//     const dateOnCards = [];
+//     const monthOnCards = [];
+    
+//     for(let i = 0; i < data.list.length; i +=8) {
+
+//       const weekDayNumber = new Date(data.list[i].dt * 1000).getDay();
+//       const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+//       dayOnCards.push(weekDays[weekDayNumber]);        
+          
+//       const dateNumber = new Date(data.list[i].dt * 1000).getDate();
+//       dateOnCards.push(dateNumber)
+        
+//       const monthNumber = new Date(data.list[i].dt * 1000).getMonth();
+//       const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec"];
+//       monthOnCards.push(months[monthNumber])
+        
+//     };
+    
+//     class Card {
+//       constructor(day, date, month) {
+//         this.day = day;
+//         this.date = date;
+//         this.month = month;
+//       };
+//     }
+
+//       const daysArray = [];
+//       for(let i = 0; i < dayOnCards.length; i +=1) {
+//         const oneDay = new Card(dayOnCards[i], dateOnCards[i], monthOnCards[i]);
+//         daysArray.push(oneDay);
+//       }
+//       console.log(daysArray);
+
+//     // const daysArray = numbers.reduce((acc, value) => acc + value, 0);
+
+
+//     test.innerHTML = markup(daysArray);
+    
+    
+//   });
+// };
+// rendMain();
+
+
+
 // import api from './api.js';
 
 // import daysCard from './daysCard.hbs';
