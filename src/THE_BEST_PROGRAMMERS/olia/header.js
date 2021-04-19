7e51984a899af9ef6a75eb493e57d8b8c23733c0
hellp <admin@email.com>
2021-04-19 10:43:14 +0300

    else commit

--- src/THE_BEST_PROGRAMMERS/olia/header.js
+++ src/THE_BEST_PROGRAMMERS/olia/header.js
@@ -1,13 +1,13 @@
 import api from './apiService.js'
-// import {render} from '../serg/index.js'
+import { render } from '../serg/index.js'
 const headerClose = document.querySelector('.header-close')
 const inputHeader = document.querySelector('.header-input')
-const cityList= document.querySelector('.header-list')
+const cityList = document.querySelector('.header-list')
 const headerform = document.querySelector('.header-form')
-formSubmit() 
-headerform.addEventListener('submit', e=>{
-    e.preventDefault();  
-    formSubmit() 
+formSubmit()
+headerform.addEventListener('submit', e => {
+    e.preventDefault();
+    formSubmit()
 })
 cityList.addEventListener('click', checkClick)
 const icons = {
@@ -32,95 +32,83 @@ const icons = {
 }
 
 //Поиск города по сабмиту
-function formSubmit(){
- let targetCity = inputHeader.value 
-    if(targetCity ===''){
-        targetCity='Kyiv' 
+function formSubmit() {
+    let targetCity = inputHeader.value
+    if (targetCity === '') {
+        targetCity = 'Kyiv'
     }
-    api.fetchWeather(targetCity) 
-    .then(data=>{
-        if(data.cod ==='404'){
-            inputHeader.value ="" 
-        }
-        const allData = {list: data.list, city: data.city}
-        allData.list.forEach(el=>{
-el.weather[0].icon = icons[el.weather[0].icon]
-el.dt = (new Date((+el.dt)*1000)).toString()
-        })
-        console.log(allData);
-    //    render(allData)
-    });
-     
-} 
+    api.fetchWeather(targetCity)
+        .then(data => {
+            if (data.cod === '404') {
+                inputHeader.value = ""
+            }
+            const allData = { list: data.list, city: data.city }
+            allData.list.forEach(el => {
+                el.weather[0].icon = icons[el.weather[0].icon]
+                el.dt = (new Date((+el.dt) * 1000)).toString()
+            })
+            console.log(allData);
+            render(allData)
+        });
+
+}
 
 //Клик на звездочку сохраняет значение в локал сторытдж и рендерит список
 // При перезагрузке сохраняются кнопки
 //Сделать проверку чтобы не возможно было добавить повторно город и написать что-либо
-        
+
 const starIcon = document.querySelector('.header-icon-star')
 starIcon.addEventListener('click', saveCity)
 
 
-function saveCity(){
-let newCity = inputHeader.value
+function saveCity() {
+    let newCity = inputHeader.value
 
-if (localStorage.getItem('city') === null){
-    localStorage.setItem('city','[]');
-}
-if (localStorage.getItem('city').includes(newCity)){
-    return
-}
-let allCity = JSON.parse(localStorage.getItem('city'))
-allCity.push(newCity);
-localStorage.setItem('city', JSON.stringify(allCity))
-createFavoritCity(newCity)
+    if (localStorage.getItem('city') === null) {
+        localStorage.setItem('city', '[]');
+    }
+    if (localStorage.getItem('city').includes(newCity)) {
+        return
+    }
+    let allCity = JSON.parse(localStorage.getItem('city'))
+    allCity.push(newCity);
+    localStorage.setItem('city', JSON.stringify(allCity))
+    createFavoritCity(newCity)
 
 }
 
-function createFavoritCity(newCity){
-let liEl = `<li class="header-item">
+function createFavoritCity(newCity) {
+    let liEl = `<li class="header-item">
     <button class="header-btn"><span class="header-text">${newCity}</span>
     <svg class="header-close">
       <use href="sprite.svg#icon-cross"></use>
   </svg>
   </button>
-  </li>`   
-cityList.insertAdjacentHTML('beforeend', liEl)    
+  </li>`
+    cityList.insertAdjacentHTML('beforeend', liEl)
 }
-if (localStorage.getItem('city') !== null){
-JSON.parse(localStorage.getItem('city')).forEach(el=>{
- createFavoritCity(el)   
-})    
+if (localStorage.getItem('city') !== null) {
+    JSON.parse(localStorage.getItem('city')).forEach(el => {
+        createFavoritCity(el)
+    })
 }
 
 
 
 //________________________________________________________________
 
-function checkClick(e){
+function checkClick(e) {
     console.dir(e)
-if(e.target.nodeName ==='svg'){
-    e.path[2].remove()
-    localStorage.removeItem('city')
+    if (e.target.nodeName === 'svg') {
+        e.path[2].remove()
+        localStorage.removeItem('city')
+    }
+    renderCityWeather(e)
 }
-renderCityWeather(e)
+
+function renderCityWeather(e) {
+    if (e.target.nodeName === 'BUTTON' || e.target.nodeName === 'SPAN') {
+        inputHeader.value = e.target.innerText
+        formSubmit()
+    }
 }
-
-function renderCityWeather(e){  
-if(e.target.nodeName ==='BUTTON' || e.target.nodeName ==='SPAN'){
-inputHeader.value =e.target.innerText
-formSubmit()
-}
-}  
-
-
-
-
-
-
-
-
-
-
-
-
