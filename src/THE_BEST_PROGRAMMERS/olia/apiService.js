b55d8fab98a45e9ba23df94143b4851c3e82aa26
SergiyStetsenko <celtik45@gmail.com>
2021-04-19 13:11:34 +0300

    6yg5t4fr3ewrfty

--- src/THE_BEST_PROGRAMMERS/olia/apiService.js
+++ src/THE_BEST_PROGRAMMERS/olia/apiService.js
@@ -1,6 +1,6 @@
 const baseUrl = `https://api.openweathermap.org/data/2.5/forecast?`
-const key = `68e2e449771b30ae0c8958ab705e7469`
-
+const key = 'c3938c5b8fa5438c627633d1d16f97b8'
+// 68e2e449771b30ae0c8958ab705e7469
 function fetchWeather(targetCity){
     return fetch(`${baseUrl}q=${targetCity}&appid=${key}`)
     .then(response=>response.json())
