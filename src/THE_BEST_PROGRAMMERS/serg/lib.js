ea660de8b7b4bbb3e9be2dc293032a402c4cf077
SergiyStetsenko <celtik45@gmail.com>
2021-04-21 00:16:38 +0300

    tre

--- src/THE_BEST_PROGRAMMERS/serg/lib.js
+++ src/THE_BEST_PROGRAMMERS/serg/lib.js
@@ -1,12 +1,14 @@
 import galeru from './galery.hbs';
-const rendMore = function (dayData) {
-    
-const button = document.querySelector('.weather-moreInfo-slider')
-button.innerHTML = `<button class="ewcw"><svg class="button_svg" width="17" height="18">
-<use href = "./sprite.svg#icon-arrow_to_right" ></use></svg></button>`
 
+const rendMore = function (dayData) {
+        const button = document.querySelector('.weather-moreInfo-slider')
+   
+    button.innerHTML = `<button class="ewcw"><svg class="button_svg" width="17" height="18">
+    <use href = "./sprite.svg#icon-arrow_to_right" ></use></svg></button>`
     const more = document.querySelector('.weather-moreInfo')
-    const pepo = dayData.splice(8)
+    const opop = dayData.splice(1,1)
+    const pepo = dayData.splice(7)
+     
     const ret = dayData.map(e => {
         const rer = e.dt.slice(16, 21)
         const wew = Math.round(e.main.pressure * 0.007500637554192211 * 100)
@@ -16,14 +18,10 @@ button.innerHTML = `<button class="ewcw"><svg class="button_svg" width="17" heig
         e.main.pressure = wew
         e.main.temp = lolo
         e.icon = icon
-        return e
-        
+        return e       
     })
     more.innerHTML = galeru(ret)
     
 }
-
-
-
 export { rendMore }
 
