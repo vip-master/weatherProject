0a8f46c738989a18cac6dc76cc72b6996d55732a
OlgaGutsuliuk <olaguculuk@gmail.com>
2021-04-20 19:04:30 +0300

    делаю ветер)

--- src/THE_BEST_PROGRAMMERS/olia/apiService.js
+++ src/THE_BEST_PROGRAMMERS/olia/apiService.js
@@ -1,8 +1,21 @@
 const baseUrl = `https://api.openweathermap.org/data/2.5/forecast?`
 const key = `411db9b3794883c087439e6ee04dd949`
 
-function fetchWeather(targetCity){
-    return fetch(`${baseUrl}q=${targetCity}&appid=${key}&units=metric`)
+function fetchWeather(options){
+    let city;
+let latitude;
+let longitude;
+    if(typeof options==='object'){
+        latitude= options.latitude
+        longitude= options.longitude
+        city =''
+    }
+    else{
+        city = options
+        latitude = ''
+        longitude = ''
+    }
+    return fetch(`${baseUrl}lat=${latitude}&lon=${longitude}&appid=${key}&q=${city}&units=metric`)
     .then(response=>response.json())
 }
 
