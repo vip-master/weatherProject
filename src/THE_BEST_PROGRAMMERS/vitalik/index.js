aeada41c4fd5b8fb31753148cb12afaa203495c6
vitalik57 <77902972+vitalik57@users.noreply.github.com>
2021-04-21 13:26:27 +0300

    start

--- src/THE_BEST_PROGRAMMERS/vitalik/index.js
+++ src/THE_BEST_PROGRAMMERS/vitalik/index.js
@@ -12,7 +12,7 @@ import Chart from 'chart.js/auto';
 function rendChart(data) {
     console.log('vitalik')
     console.log(data);
-
+    
     const grafikTemperature = data.map(temperature => {
         return temperature[0]
     })
@@ -36,16 +36,16 @@ function rendChart(data) {
         daily.push(dataGrafik[2]);
         year.push(dataGrafik[3])
     });
-  for (let i = 0; i < 5; i++) {
-   y.push(month[i]+' '+daily[i]+', '+year[i])
+    for (let i = 0; i < 5; i++) {
+        y.push(month[i] + ' ' + daily[i] + ', ' + year[i])
       
-  }
+    }
     console.log(y);
- document.querySelector('.charts').classList.add('compress')
+    // document.querySelector('.charts').classList.add('')
 
     const buttonChart = document.querySelector('.charts-open');
-    buttonChart.innerHTML = 
-   ` <button class="button-chart-close"><span class="button-chart-text">Show Chart</span>
+    buttonChart.innerHTML =
+        ` <button class="button-chart-close"><span class="button-chart-text">Show Chart</span>
         <div class="chart-svg-box">
             <svg class="chart-svg" >
                 <use href="./sprite.svg#icon-arrow_to_up"></use>
@@ -54,112 +54,87 @@ function rendChart(data) {
     </button>`;
 
     console.log(buttonChart);
-     const onButtomChartClick = document.querySelector('.button-chart-close');
+    const onButtomChartClick = document.querySelector('.button-chart-close');
     onButtomChartClick.addEventListener('click', buttonChartRemove);
 
     function buttonChartRemove() {
-        document.querySelector('.charts').classList.toggle('compress');
+        
+        // document.querySelector('.charts').classList.toggle('compress');
         // document.querySelector('.chats-canvas').classList.toggle('none');
 
         if (document.querySelector(".button-chart-text").textContent === "Show Chart") {
             document.querySelector(".button-chart-text").textContent = "Hide Chart";
-          } else {
+        } else {
             document.querySelector(".button-chart-text").textContent = "Show Chart";
-          }
-    }
-
-    Chart.defaults.font.size = 14;
-// const arr = ['22.04','23.04','24.04','25.04','26.04']
-new Chart(document.getElementById("charts-line"), {
-    type: 'line',
-    data: {
-        labels: [...y],
-        datasets: [{
-            
-            data: [...temp],
-            color: '#FFFFFF',
-            label: "— Temperature, C° ",
-            borderColor: "#FF6B09",
-            fill: false
-        }, {
-            data: [...humidity],
-            label: "— Humidity, % ",
-            borderColor: "#0906EB",
-            fill: false,
-            
-        }, {
-            data: [...windSpeed],
-            label: "— Wind Speed, m/s ",
-            borderColor: "#EA9A05",
-            fill: false
-        }, {
-            data: [...pressure],
-            label: "— Atmosphere Pressure, m/m",
-            borderColor: "#067806",
-            fill: false
         }
-        ]
-    },
-    options: {
-     
-            // responsive: true,
-            // maintainAspectRatio: true,
-            scales: {
-                y: {
-                    // display: true,
-                    // title: {
-                    //     display: true,
-                    //     text:'Value of Indicators',
-                    //     color: 'rgba(255, 255, 255, 0.5)',
-                    //     font:{
-                    //         family: 'Lato',
-                    //         size: 12,
-                    //         weight: 400,
-                    //     },
-                    //     padding:{bottom:10}
-                    // },
-                    stacked: true,
-                    beginAtZero: false,
-                    gridLines: {
-                        display: true,
-                        color: "rgba(255,99,132,0.2)",
-                    }
-                },
+    }
+    Chart.defaults.font.size = 14;
+    // const arr = ['22.04','23.04','24.04','25.04','26.04']
+    new Chart(document.getElementById("charts-line"), {
+      
+        
+        type: 'line',
+        data: {
+            labels: [...y],
+            datasets: [{
                 
-            x: {
-                gridLines: {
-                    display: false,
+                data: [...temp],
+                color: '#FFFFFF',
+                label: "— Temperature, C° ",
+                borderColor: "#FF6B09",
+                backgroundColor: '#ff6b09',
+                fill: false
+            }, {
+                data: [...humidity],
+                label: "— Humidity, % ",
+                borderColor: "#0906EB",
+                fill: false,
+            
+            }, {
+                data: [...windSpeed],
+                label: "— Wind Speed, m/s ",
+                borderColor: "#EA9A05",
+                fill: false
+            }, {
+                data: [...pressure],
+                label: "— Atmosphere Pressure, m/m",
+                borderColor: "#067806",
+                fill: false
+            }
+            ]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            plugins: {
+        title: {
+        display: true,
+        text: 'AVERAGE:',
+      }
+    },
+            responsive: true,
+            scales: {
+               
+                y: {
+                    display: true,
+                    title: {
+                        display: true,
+                        text: 'Value of Indicators',
+                      color: 'rgba(255, 255, 255, 0.5)',
+                        font: {
+                            family: 'Lato',
+                            size: 14,
+                            style: 'normal',
+                            lineHeight: 1.2,
+                            
+                        },
+                        padding: { top: 30, left: 0, right: 0, bottom: 0 }
+                    }
                 }
             }
-            },
-            plugins:{
-                legend:{
-                    position: 'top',
-                    align: 'center',
-    
-                    labels: {
-                        color: 'rgba(255, 255, 255, 0.5)',
-                        boxWidth: 10,
-                        boxHeight:10,
-                        },
-                },
-                title:{
-                    display: true,
-                    text: 'AVERAGE:',
-                    color: 'rgba(255, 255, 255, 0.5)',
-                    align: 'start',
-                    font: {
-                        size: 12,
-                        family: "Lato",
-                        weight: 400,
-                        },
-                },
-            },
-        }
-        
-    });
-
-}
+        },
+    })
+};
 // const Temperature = data.map(temp => temp[0]);
 // console.log(Temperature);
 
